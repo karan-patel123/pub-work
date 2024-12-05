aced9563180cacb92bbbdee744e00e2a28314970
pk15pi <76875206+pk15pi@users.noreply.github.com>
2024-12-05 20:29:01 +0530

    sumo sir changes added as new addtions

---
+++ vault_token_manager/ProjectInstaller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace SecureTokenService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            var processInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem // Run under LocalSystem account
+            };
+
+            // Create and configure the service installer
+            var serviceInstaller = new ServiceInstaller
+            {
+                ServiceName = "TokenVaultService", // Name of your service
+                DisplayName = "Test Token", // Display name in the Services Manager
+                Description = "Vault token caching service for offline authentication", // Description of your service
+                StartType = ServiceStartMode.Automatic // Start automatically
+            };
+
+            // Add installers to the Installers collection
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
+
---
+++ vault_token_manager/TokenVaultService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureTokenService
+{
+    public class TokenVaultService : ServiceBase
+    {
+
+        private const string CacheFilePath = @"D:\Workspace\SecureTokenService\SecureTokenService\CacheFile.txt";
+        private const string VaultUrl = "http://127.0.0.1:8200";
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public TokenVaultService()
+        {
+            this.ServiceName = "VaultManageerService";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Console.WriteLine("Service Startted");
+            // Start the service asynchronously
+            Task.Run(() => GetSecretAsync());
+        }
+
+        protected override void OnStop()
+        {
+            // Clean up resources if needed
+        }
+
+        private async Task GetSecretAsync()
+        {
+            string secret = await RetrieveFromRemoteVaultAsync() ?? RetrieveFromLocalCache();
+            Console.WriteLine(secret);
+            // Do something with the secret
+        }
+
+        private async Task<string> RetrieveFromRemoteVaultAsync()
+        {
+            try
+            {
+                var response = await httpClient.GetAsync(VaultUrl);
+                response.EnsureSuccessStatusCode();
+                string secret = await response.Content.ReadAsStringAsync();
+                CacheSecret(secret);
+                return secret;
+            }
+            catch (HttpRequestException)
+            {
+                return null;
+            }
+        }
+
+        private string RetrieveFromLocalCache()
+        {
+            if (File.Exists(CacheFilePath))
+            {
+                byte[] encryptedData = File.ReadAllBytes(CacheFilePath);
+                return DecryptSecret(encryptedData);
+            }
+            return null;
+        }
+
+        private void CacheSecret(string secret)
+        {
+            byte[] encryptedData = EncryptSecret(secret);
+            File.WriteAllBytes(CacheFilePath, encryptedData);
+        }
+
+        private byte[] EncryptSecret(string secret)
+        {
+            byte[] secretBytes = Encoding.UTF8.GetBytes(secret);
+            return ProtectedData.Protect(secretBytes, null, DataProtectionScope.LocalMachine);
+        }
+
+        private string DecryptSecret(byte[] encryptedData)
+        {
+            byte[] secretBytes = ProtectedData.Unprotect(encryptedData, null, DataProtectionScope.LocalMachine);
+            return Encoding.UTF8.GetString(secretBytes);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                httpClient.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public static void Main()
+        {
+            ServiceBase.Run(new TokenVaultService());
+        }
+    }
+}
